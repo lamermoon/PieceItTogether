bc8303c739ae87210960d1f03b89d09fe8ff2ba8
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-09 19:00:36 +0100

    Node fertig

--- SATSolver/Node.java
+++ SATSolver/Node.java
@@ -1,5 +1,33 @@
 package PieceItTogether.SATSolver;
 
-class Node {
+import java.util.ArrayList;
 
+class Node {
+	private String name;
+	private ArrayList<Node> edges = new ArrayList<Node>();
+	
+	public Node(String name, ArrayList<Node> edges){
+		this.name = name;
+		this.edges = edges;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public ArrayList<Node> getEdges(){
+		return this.edges;
+	}
+	
+	public Node getEdge(int i){
+		return edges.get(i);
+	}
+	
+	public String toString(){
+		String s = "Knoten "+name+" hat Kanten zu den Knoten:\n";
+		for(int i = 0; i < edges.size(); i++){
+			s += edges.get(i).getName() + "\n";
+		}
+		return s;
+	}
 }
