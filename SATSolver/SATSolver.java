4fbd65f87b5dd9a3b3af52411e8898f32f394b93
Marcel Wienoebst <marcel7698@gmx.de>
2015-03-10 14:02:00 +0100

    Implementierung von solve

--- SATSolver/SATSolver.java
+++ SATSolver/SATSolver.java
@@ -2,10 +2,15 @@ package PieceItTogether.SATSolver;
 
 public class SATSolver {
 	
-	
-	public boolean solve(){
-		
-		return true;
+	//Im bool-array sind a und nicht a immer nebeneinander angeordnet
+	public boolean solve(boolean[][] graph){
+		graph= transitiveClosure(graph);
+		for(int i=0; i<graph.length;i=i+2) {	//gehe durch den graph in zweierschritten
+			if(graph[i][i+1]&&graph[i+1][i]) {	//ueberpruefe, ob verbindung von a zu nicht a und vice versa
+				return false;					//dann false
+			}
+		}
+		return true;							//sonst true
 	}
 	
 	public boolean[][] transitiveClosure(boolean[][] graph) {
