98e700414c1313c91a84ef008531e33bb1494a11
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-10 14:55:20 +0100

    Warshall-Algo und Solve in SATSolver und Erweiterung von PatternSolver

--- PatternSolver/PatternSolver.java
+++ PatternSolver/PatternSolver.java
@@ -7,12 +7,28 @@ public class PatternSolver {
 	final ArrayList<Pattern> cases = new ArrayList<Pattern>(); 
 	
 	public boolean[] solve(){
+		boolean[] output = new boolean[cases.size()];
+		SATSolver sat = new SATSolver();
+		for(int i = 0; i < cases.size(); i++){
+			boolean[][] adjMtrx = reduceTo2Sat(cases.get(i));
+			output[i] = sat.solve(adjMtrx);
+		}
+		return output;
+	}
+	
+	private boolean[][] reduceTo2Sat(Pattern p){
+		boolean[][] adjMtrx = new boolean[2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())][2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())]; //2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM()) ergibt die Anzahl der Kanten im Pattern
 		
-		
-		String s ="";
-		SATSolver sat = new SATSolver(s);
-		boolean[] x = {true,false,false,true};
-		return x;
+		for(int i = 0; i < adjMtrx.length; i++){
+			for(int j = 0; j < adjMtrx.length; j++){
+				if(adjMtrx[i][j]){
+					//hier passiert nichts
+				}else{
+					adjMtrx[i][j] = false;
+				}
+			}
+		}
+		return adjMtrx;
 	}
 	
 	public void addPattern(Pattern p){
--- SATSolver/SATSolver.java
+++ SATSolver/SATSolver.java
@@ -1,16 +1,28 @@
 package PieceItTogether.SATSolver;
 
-import java.util.ArrayList;
-
 public class SATSolver {
-	private ArrayList<Node> graph = new ArrayList<Node>();
 	
-	public SATSolver(String s){
+	public boolean solve(boolean[][] adjMtrx){
+		calcTransClosure(adjMtrx);
+		for(int i = 0; i < adjMtrx.length; i=i+2){
+			if(adjMtrx[i][i+1] && adjMtrx[i+1][i]){
+				return false;
+			}
+		}
+		return true;
 		
 	}
 	
-	public boolean solve(){
-		
-		return true;
+	private void calcTransClosure(boolean[][] adjMtrx){
+		for(int i = 0; i < adjMtrx.length; i++){
+			adjMtrx[i][i] = true;
+		}
+		for(int i = 0; i < adjMtrx.length; i++){
+			for(int j = 0; j < adjMtrx.length; j++){
+				for(int k = 0; k < adjMtrx.length; k++){
+					adjMtrx[j][k] = adjMtrx[j][k] || (adjMtrx[j][i] && adjMtrx[i][k]);
+				}
+			}
+		}
 	}
 }
