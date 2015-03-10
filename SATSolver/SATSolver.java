1601f76006b689d08017b5f65b79a3ca2a080e42
Marcel Wienoebst <marcel7698@gmx.de>
2015-03-10 13:41:00 +0100

    Warshall Algorithmus zur Berechnung der transitiven Huelle implementiert.

--- SATSolver/SATSolver.java
+++ SATSolver/SATSolver.java
@@ -7,4 +7,18 @@ public class SATSolver {
 		
 		return true;
 	}
+	
+	public boolean[][] transitiveClosure(boolean[][] graph) {
+		for (int v=0; v<graph.length; v++)
+			graph[v][v] = true;
+		for (int v=0; v<graph.length; v++) {
+			for (int i=0; i<graph.length; i++) {
+				for (int j=0; j<graph.length; j++) {
+					graph[i][j]= graph[i][j] || (graph[i][v]&&graph[v][i]);
+				}
+					
+			}
+		}
+		return graph;
+	}
 }
