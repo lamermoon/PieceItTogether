539b4a6b88aa4715f58fe2db35bceb2edcee78d0
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-10 21:04:02 +0100

    neuer Testinput, Weiterarbeit an PatternSolver

--- PatternSolver/PatternSolver.java
+++ PatternSolver/PatternSolver.java
@@ -17,16 +17,41 @@ public class PatternSolver {
 	}
 	
 	private boolean[][] reduceTo2Sat(Pattern p){
-		boolean[][] adjMtrx = new boolean[2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())][2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())]; //2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM()) ergibt die Anzahl der Kanten im Pattern
-		
-		for(int i = 0; i < adjMtrx.length; i++){
-			for(int j = 0; j < adjMtrx.length; j++){
-				if(adjMtrx[i][j]){
-					//hier passiert nichts
-				}else{
-					adjMtrx[i][j] = false;
+		int black = 0;
+		int white = 0;
+		char[][] pattern = p.getPattern();
+	/* Zaehle Anzahl der schwarzen und weissen Felder */
+		for(int i = 0; i < p.getN(); i++){
+			for(int j = 0; j < p.getM(); j++){
+				if(pattern[i][j] == 'W'){
+					white++;
+				}else if(pattern[i][j] == 'B'){
+					black++;
 				}
 			}
+		}
+		boolean[][] adjMtrx;
+	/* Wenn doppelt so viele weisse wie schwarze Felder da sind, ist eine Loesung moeglich */
+		if(2*black == white){
+			
+			adjMtrx = new boolean[2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())][2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM())]; //2*(p.getN()*(p.getM()-1)+(p.getN()-1)*p.getM()) ergibt die Anzahl der Kanten im Pattern
+			
+			for(int i = 0; i < adjMtrx.length; i++){
+				for(int j = 0; j < adjMtrx.length; j++){
+					if(adjMtrx[i][j]){
+						//hier passiert nichts
+					}else{
+						adjMtrx[i][j] = false;
+					}
+				}
+			}
+	/* Sonst nicht */	
+		} else {
+			boolean[] a = {true, true};
+			boolean[] b = {true, true};
+			adjMtrx = new boolean[2][2];
+			adjMtrx[0] = a;
+			adjMtrx[1] = b;
 		}
 		return adjMtrx;
 	}
