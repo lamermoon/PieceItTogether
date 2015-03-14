1d38efb92f3c1017fa70007d91a9ac3be3a798b9
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-14 17:23:37 +0100

    Grundlegenden Fehler in Methode transitiveClosure() behoben

--- SATSolver/SATSolver.java
+++ SATSolver/SATSolver.java
@@ -20,7 +20,7 @@ public class SATSolver {
 		for (int v=0; v<graph.length; v++) {
 			for (int i=0; i<graph.length; i++) {
 				for (int j=0; j<graph.length; j++) {
-					graph[i][j]= graph[i][j] || (graph[i][v]&&graph[v][i]);
+					graph[i][j]= graph[i][j] || (graph[i][v]&&graph[v][j]);
 				}
 			}
 		}
