3b14d019b10c92fb472545919a4e1cee17b8c192
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-15 21:15:19 +0100

    alles FERTIG, fehlt noch ausgiebiges Testen

--- PatternSolver/PatternSolver.java
+++ PatternSolver/PatternSolver.java
@@ -136,6 +136,300 @@ public class PatternSolver {
 					/* Wenn das aktuelle Feld ein weisses Feld ist: */
 						} else {
 							//TODO: Faelle abarbeiten
+							//Erster Fall: 4 schwarze Nachbarn
+							if(left == 'B' && right == 'B' && top == 'B' && down == 'B'){
+							//(nicht a oder nicht b) und (nicht b oder nicht c) und (nicht a oder nicht c) und (nicht d oder nicht b) und (nicht d oder nicht a) und (nicht d oder nicht c)
+								//(a impliziert nicht b)
+								adjMtrx[a][nb] = true;
+								//(b impliziert nicht a)
+								adjMtrx[b][na] = true;
+								//(b impliziert nicht c)
+								adjMtrx[b][nc] = true;
+								//(c impliziert nicht b)
+								adjMtrx[c][nb] = true;
+								//(a impliziert nicht c)
+								adjMtrx[a][nc] = true;
+								//(c impliziert nicht a)
+								adjMtrx[c][na] = true;
+								//(d impliziert nicht a)
+								adjMtrx[d][na] = true;
+								//(a impliziert nicht d)
+								adjMtrx[a][nd] = true;
+								//(d impliziert nicht b)
+								adjMtrx[d][nb] = true;
+								//(b impliziert nicht d)
+								adjMtrx[b][nd] = true;
+								//(d impliziert nicht c)
+								adjMtrx[d][nc] = true;
+								//(c impliziert nicht d)
+								adjMtrx[c][nd] = true;
+							}
+							//Zweiter Fall: 3 schwarze Nachbarn
+							else if((left == 'B' && top == 'B' && right == 'B') ||
+									(down == 'B' && top == 'B' && right == 'B') ||
+									(down == 'B' && left == 'B' && right == 'B') ||
+									(down == 'B' && left == 'B' && top == 'B')){
+								if(left == 'W' || left == '.'){
+								//(nicht a oder nicht b) und (nicht b oder nicht c) und (nicht c oder nicht a) [und (nicht d oder nicht d)]
+									//(a impliziert nicht b)
+									adjMtrx[a][nb] = true;
+									//(b impliziert nicht a)
+									adjMtrx[b][na] = true;
+									//(b impliziert nicht c)
+									adjMtrx[b][nc] = true;
+									//(c impliziert nicht b)
+									adjMtrx[c][nb] = true;
+									//(a impliziert nicht c)
+									adjMtrx[a][nc] = true;
+									//(c impliziert nicht a)
+									adjMtrx[c][na] = true;
+									if(left == 'W'){
+										//(d impliziert nicht d)
+										adjMtrx[d][nd] = true;
+									}
+								}else if(right == 'W' || right == '.'){
+									//(nicht a oder nicht b) und (nicht b oder nicht d) und (nicht d oder nicht a) [und (nicht c oder nicht c)]
+									//(a impliziert nicht b)
+									adjMtrx[a][nb] = true;
+									//(b impliziert nicht a)
+									adjMtrx[b][na] = true;
+									//(b impliziert nicht d)
+									adjMtrx[b][nd] = true;
+									//(d impliziert nicht b)
+									adjMtrx[d][nb] = true;
+									//(a impliziert nicht d)
+									adjMtrx[a][nd] = true;
+									//(d impliziert nicht a)
+									adjMtrx[d][na] = true;
+									if(right == 'W'){
+										//(c impliziert nicht c)
+										adjMtrx[c][nc] = true;
+									}
+								}else if(top == 'W' || top == '.'){
+									//(nicht a oder nicht d) und (nicht d oder nicht c) und (nicht c oder nicht a) [und (nicht b oder nicht b)]
+									//(a impliziert nicht d)
+									adjMtrx[a][nd] = true;
+									//(d impliziert nicht a)
+									adjMtrx[d][na] = true;
+									//(d impliziert nicht c)
+									adjMtrx[d][nc] = true;
+									//(c impliziert nicht d)
+									adjMtrx[c][nd] = true;
+									//(a impliziert nicht c)
+									adjMtrx[a][nc] = true;
+									//(c impliziert nicht a)
+									adjMtrx[c][na] = true;
+									if(top == 'W'){
+										//(b impliziert nicht b)
+										adjMtrx[b][nb] = true;
+									}
+								}else if(down == 'W' || down == '.'){
+									//(nicht d oder nicht b) und (nicht b oder nicht c) und (nicht c oder nicht d) [und (nicht a oder nicht a)]
+									//(d impliziert nicht b)
+									adjMtrx[d][nb] = true;
+									//(b impliziert nicht d)
+									adjMtrx[b][nd] = true;
+									//(b impliziert nicht c)
+									adjMtrx[b][nc] = true;
+									//(c impliziert nicht b)
+									adjMtrx[c][nb] = true;
+									//(d impliziert nicht c)
+									adjMtrx[d][nc] = true;
+									//(c impliziert nicht d)
+									adjMtrx[c][nd] = true;
+									if(top == 'W'){
+										//(a impliziert nicht a)
+										adjMtrx[a][na] = true;
+									}
+								}
+							}
+							//Dritter Fall: 2 schwarze Nachbarn
+							else if(right == 'B' && down == 'B'){
+							//(c oder d) und (nicht c oder nicht d) [und (nicht a oder nicht a)] [und (nicht b oder nicht b)]
+								//(nicht c impliziert d)
+								adjMtrx[nc][d] = true;
+								//(nicht d impliziert c)
+								adjMtrx[nd][c] = true;
+								//(c impliziert nicht d)
+								adjMtrx[c][nd] = true;
+								//(d impliziert nicht c)
+								adjMtrx[d][nc] = true;
+								if(left == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+								if(top == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+							}
+							else if(top == 'B' && down == 'B'){
+							//(b oder d) und (nicht b oder nicht d) [und (nicht a oder nicht a)] [und (nicht c oder nicht c)]
+								//(nicht b impliziert d)
+								adjMtrx[nb][d] = true;
+								//(nicht d impliziert b)
+								adjMtrx[nd][b] = true;
+								//(b impliziert nicht d)
+								adjMtrx[b][nd] = true;
+								//(d impliziert nicht b)
+								if(left == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+								if(top == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}	
+							}
+							else if(top == 'B' && right == 'B'){
+							//(c oder b) und (nicht c oder nicht b) [und (nicht a oder nicht a)] [und (nicht d oder nicht d)]
+								//(nicht c impliziert b)
+								adjMtrx[nc][b] = true;
+								//(nicht b impliziert c)
+								adjMtrx[nb][c] = true;
+								//(c impliziert nicht b)
+								adjMtrx[c][nb] = true;
+								//(b impliziert nicht c)
+								adjMtrx[b][nc] = true;
+								if(left == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+								if(top == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}	
+							}
+							else if(left == 'B' &&  down == 'B'){
+							//(a oder d) und (nicht a oder nicht d) [und (nicht b oder nicht b)] [und (nicht c oder nicht c)]
+								//(nicht a impliziert d)
+								adjMtrx[na][d] = true;
+								//(nicht d impliziert a)
+								adjMtrx[nd][a] = true;
+								//(a impliziert nicht d)
+								adjMtrx[a][nd] = true;
+								//(d impliziert nicht a)
+								adjMtrx[d][na] = true;
+								if(left == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+								if(top == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}
+							}
+							else if(left == 'B' && right == 'B'){
+							//(a oder c) und (nicht a oder nicht c) [und (nicht b oder nicht b)] [und (nicht d oder nicht d)]
+								//(nicht a impliziert c)
+								adjMtrx[na][c] = true;
+								//(nicht c impliziert a)
+								adjMtrx[nc][a] = true;
+								//(a impliziert nicht c)
+								adjMtrx[a][nc] = true;
+								//(c impliziert nicht a)
+								adjMtrx[c][na] = true;
+								if(left == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+								if(top == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}
+							}
+							else if(left == 'B' && top == 'B'){
+							//(a oder b) und (nicht a oder nicht b) [und (nicht c oder nicht c)] [und (nicht d oder nicht d)]
+								//(nicht a impliziert b)
+								adjMtrx[na][b] = true;
+								//(nicht b impliziert a)
+								adjMtrx[nb][a] = true;
+								//(a impliziert nicht b)
+								adjMtrx[a][nb] = true;
+								//(b impliziert nicht a)
+								adjMtrx[b][na] = true;
+								if(left == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}
+								if(top == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}
+							}
+							//Vierter Fall: 1 schwarzer Nachbar
+							else if(left == 'B'){
+							//(a oder a) [und (nicht b oder nicht b)] [und (nicht c oder nicht c)] [und (nicht d oder nicht d)]
+								//(nicht a impliziert a)
+								adjMtrx[na][a] = true;
+								if(top == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+								if(right == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}
+								if(down == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}
+							}
+							else if(right == 'B'){
+							//(c oder c) [und (nicht b oder nicht b)] [und (nicht a oder nicht a)] [und (nicht d oder nicht d)]
+								//(nicht c impliziert c)
+								adjMtrx[nc][c] = true;
+								if(top == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+								if(right == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+								if(down == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}	
+							}
+							else if(top == 'B'){
+							//(b oder b) [und (nicht a oder nicht a)] [und (nicht c oder nicht c)] [und (nicht d oder nicht d)]
+								//(nicht b impliziert b)
+								adjMtrx[nb][b] = true;
+								if(top == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+								if(right == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}
+								if(down == 'W'){
+									//(d impliziert nicht d)
+									adjMtrx[d][nd] = true;
+								}
+							}
+							else if(down == 'B'){
+							//(d oder d) [und (nicht b oder nicht b)] [und (nicht c oder nicht c)] [und (nicht a oder nicht a)]
+								//(nicht d impliziert d)
+								adjMtrx[nd][d] = true;
+								if(top == 'W'){
+									//(b impliziert nicht b)
+									adjMtrx[b][nb] = true;
+								}
+								if(right == 'W'){
+									//(c impliziert nicht c)
+									adjMtrx[c][nc] = true;
+								}
+								if(down == 'W'){
+									//(a impliziert nicht a)
+									adjMtrx[a][na] = true;
+								}
+							}
+							//Fünfter Fall: 0 schwarze Nachbarn
+							else{
+								return falseSatInstance();
+							}
 						}
 					}
 					
