847e7ddf0698dec3a06bbe048da7014fa97d31d3
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-16 17:02:03 +0100

    erneuter Rename

--- MainFrame.java
+++ MainFrame.java
@@ -5,10 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
 import PieceItTogether.PatternSolver.PatternSolver;
 import PieceItTogether.PatternSolver.Pattern;
 
-public class MainPanel {
+public class MainFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) throws NumberFormatException, IOException{
 			PatternSolver ps = new PatternSolver();
