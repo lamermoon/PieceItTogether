3513e019ec1f7db5a0b1ca58968db1d3819f2604
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2015-03-17 18:05:43 +0100

    GUI erweitert, Fehlertoleranz geteigert

--- MainFrame.java
+++ MainFrame.java
@@ -2,6 +2,7 @@ package PieceItTogether;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -20,10 +21,10 @@ import PieceItTogether.PatternSolver.Pattern;
 
 public class MainFrame extends JFrame implements ActionListener{
 	private JButton button;
-	private JSplitPane center;
-	private JPanel top, bottom;
-	private JTextArea inputArea, outputArea;
-	private JScrollPane isp, osp;
+	private JSplitPane ioPanel, center;
+	private JPanel top, bottom, subCenter;
+	private JTextArea inputArea, outputArea, errorArea;
+	private JScrollPane isp, osp, esp;
 	private JProgressBar progBar;
 	private static final long serialVersionUID = 1L;
 	
@@ -35,19 +36,30 @@ public class MainFrame extends JFrame implements ActionListener{
 		this.button.addActionListener(this);
 		
 		this.inputArea = new JTextArea();
-		this.inputArea.setSize(750, 800);
+		this.inputArea.setFont(new Font(Font.MONOSPACED, 5, 16));
+		this.inputArea.setSize(750, 700);
 		this.inputArea.setMinimumSize(new Dimension(100, 100));
 		this.inputArea.setVisible(true);
 		
 		this.isp = new JScrollPane(inputArea);
 		
 		this.outputArea = new JTextArea();
-		this.outputArea.setSize(100, 800);
+		this.outputArea.setFont(new Font(Font.MONOSPACED, 5, 16));
+		this.outputArea.setSize(250, 700);
 		this.outputArea.setMinimumSize(new Dimension(100, 100));
 		this.outputArea.setEditable(false);
 		this.outputArea.setVisible(true);
 		
 		this.osp = new JScrollPane(outputArea);
+		
+		this.errorArea = new JTextArea();
+		this.errorArea.setFont(new Font(Font.MONOSPACED, 5, 16));
+		this.errorArea.setSize(1000, 100);
+		this.errorArea.setMinimumSize(new Dimension(1000, 50));
+		this.errorArea.setEditable(false);
+		this.errorArea.setVisible(true);
+		
+		this.esp = new JScrollPane(errorArea);
 		
 		//TODO: ProgressBar zeigt Fortschritt an
 		this.progBar = new JProgressBar();
@@ -58,8 +70,16 @@ public class MainFrame extends JFrame implements ActionListener{
 		this.top.add(new JLabel(" INPUT:"), BorderLayout.WEST);
 		this.top.add(new JLabel("OUTPUT: "), BorderLayout.EAST);
 		
-		this.center = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, isp, osp);
-		this.center.setDividerLocation(inputArea.getWidth());
+		this.ioPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, isp, osp);
+		this.ioPanel.setDividerLocation(inputArea.getWidth());
+		
+		this.subCenter = new JPanel();
+		this.subCenter.setLayout(new BorderLayout());
+		this.subCenter.add(new JLabel("ERROR Output:"), BorderLayout.NORTH);
+		this.subCenter.add(esp, BorderLayout.CENTER);
+		
+		this.center = new JSplitPane(JSplitPane.VERTICAL_SPLIT, ioPanel, subCenter);
+		this.center.setDividerLocation(inputArea.getHeight()-100);
 		
 		this.bottom = new JPanel();
 		this.bottom.add(button);
@@ -75,21 +95,18 @@ public class MainFrame extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-
-	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		PatternSolver ps = new PatternSolver();
 		
 		try{
 			init(ps);
-		} catch (IOException e){
-			printError(4);
+			boolean[] b = ps.solve();
+			this.outputArea.setText("");
+			for(int i = 0; i < b.length; i++){
+				this.outputArea.append(b[i] ? "Yes\n" : "No\n");
+			}
 		}
-		boolean[] b = ps.solve();
-		this.outputArea.setText("");
-		for(int i = 0; i < b.length; i++){
-			this.outputArea.append(b[i] ? "Yes\n" : "No\n");
-		}
+		catch (IOException e){printError(0);}
 	}
 	
 	public static void main(String[] args) throws Exception{
@@ -97,59 +114,68 @@ public class MainFrame extends JFrame implements ActionListener{
 		f.setVisible(true);
 	}
 	
-	private void init(PatternSolver ps) throws NumberFormatException, IOException{
-		//TODO: Fehleranfälligkeit verringern (zu viele Abstürze bei fehlerhaften Eingaben)
+	private void init(PatternSolver ps) throws IOException{
+		//TODO: Fehleranfaelligkeit verringern (zu viele Abstuerze bei fehlerhaften Eingaben)
 		String[] input = inputArea.getText().split("\\n");
 		char[][] pattern;
-		int k = Integer.parseInt(input[0]);
-		int n;
-		int m;
-		int pos = 1;
-		if(k <= 100){
-		  	if(k > 0){
-		   		for(int i = 0; i < k; i++){
-		    		String s = input[pos++];
-		    		int a = s.indexOf(" ");
-		    		n = Integer.parseInt(s.substring(0, a));
-		    		m = Integer.parseInt(s.substring(a+1));
-		    		
-		    		if(1 <= n && 500 >= n  &&  1 <= m && 500 >= m){
-			    		pattern = new char[n][];
+		try{
+			int k = Integer.parseInt(input[0]);
+			int n;
+			int m;
+			int pos = 1;
+			if(k <= 100){
+			  	if(k > 0){
+			   		for(int i = 0; i < k; i++){
+			    		String s = input[pos++];
+			    		int a = s.indexOf(" ");
+			    		try{
+			    			n = Integer.parseInt(s.substring(0, a));
+			    			m = Integer.parseInt(s.substring(a+1));
 			    		
-			    		for(int j = 0; j < n; j++){
-			    			char[] c = input[pos++].toCharArray();
-			    			pattern[j] = c;
-			    		}
-			    		Pattern p = new Pattern(n, m, pattern);
-			    		ps.addPattern(p);
-		    		} else {
-		    			System.out.println("The dimension of test case "+i+"is wrong.\n"
-		    					+ "The given patterns must be at least 1x1 and not greater than 500 in width or hight.\n"
-		    					+ "Test case "+i+" will be ignored.");
-		    		}
-		    	}			    		
-		   	} else {printError(3);}
-		} else {printError(2);}
+				    		if(1 <= n && 500 >= n  &&  1 <= m && 500 >= m){
+					    		pattern = new char[n][];
+					    		
+					    		for(int j = 0; j < n; j++){
+					    			char[] c = input[pos++].toCharArray();
+					    			pattern[j] = c;
+					    		}
+					    		Pattern p = new Pattern(n, m, pattern);
+					    		ps.addPattern(p);
+					    		
+				    		} else {
+				    			this.errorArea.setText("ALERT: The dimension of test case "+(i+1)+"is wrong.\n"
+				    					+ "The given patterns must be at least 1x1 and not greater than 500 in eather width or hight.\n"
+				    					+ "Test case "+i+" will be ignored.");
+				    		}
+			    		} 
+			    		catch(NumberFormatException e){printError(5);}
+			    	}
+			   	} else {printError(3);}
+			} else {printError(2);}
+		} 
+		catch(NumberFormatException e){printError(1);} 
+		catch(StringIndexOutOfBoundsException e){printError(4);}
 	}
 	
 	private void printError(int n){
-		String s = "";
+		String s;
 		switch(n){
-			case 0:	s = "Please specify a file path.";
+			case 0: s = "ERROR: IOException occured during init().";
 					break;
-			case 1: s = "File not found, please try again!\n"
-					+ "Input must be the path to a file in .txt format containing the input";
+			case 1: s = "ERROR: First line must contain a number between 0 and 100!";
 					break;
-			case 2: s = "The amount of test cases must not be greater than 100!";
+			case 2: s = "ERROR: The amount of test cases must not exceed 100!";
 					break;
-			case 3: s = "No test cases specified!";
+			case 3: s = "ERROR: No test cases specified!";
 					break;
-			case 4: s = "IOException occured during init().";
+			case 4: s = "ERROR: Format of input incorrect. Please check your input.";
+					break;
+			case 5: s = "ERROR: First line of each pattern must contain hight and width of the pattern separated by a blank sysmbol";
 			
 			default: s = "An error has occured. Please restart the programm.";
 					break;
 		}
-		System.err.println(s);
-		System.exit(1);
+		this.errorArea.setText(s);
+		//System.out.println(s);
 	}
 }
