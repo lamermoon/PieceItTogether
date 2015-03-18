b6116c0f57653c7ca00c920a0bab96f288e43ed1
Marcel Wienoebst <marcel7698@gmx.de>
2015-03-18 14:10:56 +0100

    Kommentar angepasst

--- SATSolver/SATSolver.java
+++ SATSolver/SATSolver.java
@@ -9,7 +9,7 @@ public class SATSolver {
 		adjList= transitiveClosure(adjList);
 		for(int i=0;i<adjList.length;i=i+2) {			//gehe durch alle "nichtnegierten Listen"
 			int j=0;
-			while(adjList[i].getEdge(j)!=null) {	//erste Liste
+			while(adjList[i].getEdge(j)!=null) {	//i-te Liste
 				if(adjList[i].getEdge(j).equals(adjList[i+1])) {	//schaue, ob Implikation zu negierter Liste
 					int k=0;
 					while(adjList[i+1].getEdge(k)!=null) {			//falls ja , gehe durch negierte Liste
